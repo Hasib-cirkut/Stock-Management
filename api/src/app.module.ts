bef9d1521172f1cd95ccff44d23bdacbeecc0831
Hasibul Huda <hasibulhuda1@gmail.com>
2023-04-30 21:33:34 +0600

    [api] update prisma schema to have order entity and add docker-compose to run mysql locally

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
-
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, OrderModule],
   controllers: [],
   providers: [],
 })
